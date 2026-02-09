1e157ad37fd312f7c96a8a2a3791a0dabd77b791
raamy24a <raamydib@gmail.com>
2026-02-09 03:22:48 +0100

    a

--- CPP00/ex01/MyAwesomePhoneBook.cpp
+++ CPP00/ex01/MyAwesomePhoneBook.cpp
@@ -6,61 +6,87 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:55:01 by radib             #+#    #+#             */
-/*   Updated: 2026/02/06 14:15:54 by radib            ###   ########.fr       */
+/*   Updated: 2026/02/09 03:21:07 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MyAwesomePhoneBook.hpp"
 
-int    add(PhoneBook *my_phone_book, int i)
+bool input(std::string &destination, std::string label)
 {
-    std::cout << "first name:";
-    std::getline(std::cin, (*my_phone_book).contact_list[i].first_name);
-    std::cout << "last name:";
-    std::getline(std::cin, (*my_phone_book).contact_list[i].last_name);
-    std::cout << "nickname:";
-    std::getline(std::cin, (*my_phone_book).contact_list[i].nickname);
-    std::cout << "phone number:";
-    std::getline(std::cin, (*my_phone_book).contact_list[i].phone_number);
-    std::cout << "darkest secret:";
-    std::getline(std::cin, (*my_phone_book).contact_list[i].darkest_secret);
-    return (1);
+	std::cout << label;
+	std::getline(std::cin, destination);
+	if (std::cin.eof())
+		return (false);
+	return !destination.empty();
 }
-void    search(PhoneBook my_phone_book)
+Optional add()
 {
-    int i = 0; 
-    while(my_phone_book.contact_list[i].first_name != "")
-    {
-        std::cout << "|";
-        std::cout << std::setw(10) << i;
-        std::cout << "|";
-        std::cout << std::setw(10) << my_phone_book.contact_list[i].first_name;
-        std::cout << "|";
-        std::cout << std::setw(10) << my_phone_book.contact_list[i].last_name;
-        std::cout << "|";
-        std::cout << std::setw(10) << my_phone_book.contact_list[i].nickname;
-        std::cout << "|" << std::endl;
-        i++;
-    }
+	Optional new_contact;
+	new_contact.has_value = false;
 
+	if (!input(new_contact.val.first_name, "first name:\n"))
+		return new_contact;
+	if (!input(new_contact.val.last_name, "last name:\n"))
+		return new_contact;
+	if (!input(new_contact.val.nickname, "nickname:\n"))
+		return new_contact;
+	if (!input(new_contact.val.phone_number, "phone number:\n"))
+		return new_contact;
+	if (!input(new_contact.val.darkest_secret, "darkest secret:\n"))
+		return new_contact;
+	new_contact.has_value = true;
+	return new_contact;
+}
+void search(PhoneBook my_phone_book)
+{
+	int i = 0;
+	while (i < 8)
+	{
+		std::cout << std::setw(10) << i;
+		std::cout << "|";
+		if (my_phone_book.contact_list[i].first_name.length() > 10)
+			std::cout << my_phone_book.contact_list[i].first_name.substr(0, 9) << '.';
+		else
+			std::cout << std::setw(10) << my_phone_book.contact_list[i].first_name.substr(0, 10);
+		std::cout << "|";
+		if (my_phone_book.contact_list[i].last_name.length() > 10)
+			std::cout << my_phone_book.contact_list[i].last_name.substr(0, 9) << '.';
+		else
+			std::cout << std::setw(10) << my_phone_book.contact_list[i].last_name.substr(0, 10);
+		std::cout << "|";
+		if (my_phone_book.contact_list[i].nickname.length() > 10)
+			std::cout << my_phone_book.contact_list[i].nickname.substr(0, 9) << '.' << std::endl;
+		else
+			std::cout << std::setw(10) << my_phone_book.contact_list[i].nickname.substr(0, 10) << std::endl;;
+		i++;
+	}
 }
 
 int main(void)
 {
-    std::string temp;
-    int         index = 0;
-    PhoneBook   my_phone_book;
-    while (1)
-    {
-        std::cout << "Choose between : ADD, SEARCH, EXIT" << std::endl;
-        std::getline(std::cin, temp);
-        if (temp !="ADD" && temp !="SEARCH" && temp !="EXIT")
-            std::cout << "Wrong argument" << std::endl;
-        else if (temp =="ADD")
-            index += add(&my_phone_book, index);
-        else if (temp =="SEARCH")
-            search(my_phone_book);
-        else if (temp =="EXIT")
-            return (0);
-    }
+	std::string temp;
+	PhoneBook 	my_phone_book;
+	Optional	temporary;
+	int			index;
+
+	while (!std::cin.eof())
+	{
+		input(temp, "Choose between : ADD, SEARCH, EXIT\n");
+		if (temp != "ADD" && temp != "SEARCH" && temp != "EXIT" && !std::cin.eof())
+			std::cout << "Wrong argument" << std::endl;
+		else if (temp == "ADD")
+		{
+			temporary = add();
+			if (temporary.has_value)
+			{
+				my_phone_book.contact_list[index % 8] = temporary.val;
+				index++;
+			}
+		}
+		else if (temp == "SEARCH")
+			search(my_phone_book);
+		else if (temp == "EXIT")
+			return (0);
+	}
 }
--- CPP00/ex01/MyAwesomePhoneBook.hpp
+++ CPP00/ex01/MyAwesomePhoneBook.hpp
@@ -6,17 +6,19 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:54:58 by radib             #+#    #+#             */
-/*   Updated: 2026/02/06 13:29:54 by radib            ###   ########.fr       */
+/*   Updated: 2026/02/07 01:39:42 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <optional>
 
 #ifndef MYAWSOMEPHONEBOOK_HPP
 #define MYAWSOMEPHONEBOOK_HPP
 #endif
+
 
 class Contact {
 public:
@@ -34,4 +36,9 @@ public:
     Contact contact_list[8];
 private:
     
+};
+
+struct Optional {
+    Contact val;
+    bool has_value;
 };
