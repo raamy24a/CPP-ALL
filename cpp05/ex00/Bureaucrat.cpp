1404fb29107fbaa256ed061798c57b96357c4f14
raamy24a <radib@fu-r2-p8.s19.be>
2026-06-02 13:44:35 +0200

    a

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,7 +6,34 @@
 /*   By: radib <radib@student.42belgium.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 11:24:39 by radib             #+#    #+#             */
-/*   Updated: 2026/06/01 11:24:40 by radib            ###   ########.fr       */
+/*   Updated: 2026/06/02 11:49:08 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+{
+	try
+	{
+		if (grade <= 0)
+			throw(GradeTooHighException());
+		else if (grade >= 151)
+			throw(GradeTooLowException());
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	_grade = grade;
+	
+}
+Bureaucrat::~Bureaucrat(){
+	
+}
+char const *Bureaucrat::GradeTooHighException::what(void) const throw(){
+	return "Grade is too high, try a value between 1 and 150";
+}
+char const *Bureaucrat::GradeTooLowException::what(void) const throw(){
+	return "Grade is too low, try a value between 1 and 150";
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,27 @@
 /*   By: radib <radib@student.42belgium.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 11:24:41 by radib             #+#    #+#             */
-/*   Updated: 2026/06/01 11:25:37 by radib            ###   ########.fr       */
+/*   Updated: 2026/06/02 11:52:26 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+	public :
+	~Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	private :
+	const std::string 	_name;
+	int 				_grade;
+	class GradeTooHighException : public std::exception {
+		public:
+			virtual char const	*what(void) const throw();
+	};
+	class GradeTooLowException : public std::exception {
+		public:
+			virtual char const	*what(void) const throw();
+	};
+} ;
