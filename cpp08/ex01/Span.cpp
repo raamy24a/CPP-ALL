eeea9abbd8cf7ebfe89aa6035935beffcfc37c5a
raamy24a <raamydib@gmail.com>
2026-08-08 03:08:30 +0200

    a

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,37 +6,69 @@
 /*   By: radib <radib@student.42belgium.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 04:34:54 by radib             #+#    #+#             */
-/*   Updated: 2026/08/07 05:25:59 by radib            ###   ########.fr       */
+/*   Updated: 2026/08/08 02:56:07 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
+char const *Span::alreadyFull::what(void) const throw()
+{
+	return "The Span is already full";
+}
+
+char const *Span::not_enough::what(void) const throw()
+{
+	return "not enough numbers yet add more";
+}
+
 Span::Span(unsigned int N)
 {
     int *p = new int[N];
     _storage = p;
-    _short = 0;
+    _short = 90000000000;
     _long = 0;
     _storedCount = 0;
-    _maxStorage = N - 1;
+    _small = 0;
+    _big = 0;
+    _maxStorage = N;
 }
 void Span::addNumber(int i)
 {
-    if (_storedCount => _maxStorage)
-        throw already_full;
+    if (_storedCount >= _maxStorage)
+        throw alreadyFull();
     _storage[_storedCount] = i;
     _storedCount++;
+    int count = 0;
+    if (_storedCount == 1)
+    {
+        _small = i;
+        _big = i;
+    }
+    if (_small > i)
+        _small = i;
+    if (_big < i)
+        _big = i;
+
+    while (count < _storedCount - 1)
+    {
+        if (i < _storage[count] && _storage[count] - i < _short)
+            _short = _storage[count] - i;
+        else if (_storage[count] - i < _short)
+            _short = i - _storage[count];
+        count++;
+    }
+    _long = _big - _small;
 }
 int Span::shortestSpan()
 {
     if (_storedCount <= 1)
-        throw not_enough;
+        throw not_enough();
     return (_short);
 }
 int Span::longestSpan()
 {
     if (_storedCount <= 1)
-        throw not_enough;
+        throw not_enough();
     return (_long);
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,24 +6,45 @@
 /*   By: radib <radib@student.42belgium.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 04:35:51 by radib             #+#    #+#             */
-/*   Updated: 2026/08/07 05:23:54 by radib            ###   ########.fr       */
+/*   Updated: 2026/08/08 02:58:31 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
+#include <stdexcept>
+#include <limits>
 
 class Span
 {
     private :
         int *_storage;
-        int _short;
-        int _long;
+        unsigned long long _short;
+        unsigned long long _long;
         int _storedCount;
         int _maxStorage;
+        int _small;
+        int _big;
     public :
         Span(unsigned int N);
         ~Span();
         void addNumber(int i);
         int shortestSpan();
         int longestSpan();
-} ;
+        class alreadyFull : public std::exception {
+		public:
+			virtual char const	*what(void) const throw();
+        } ;
+        class not_enough : public std::exception {
+                public:
+                    virtual char const	*what(void) const throw();
+        } ;
+        template <typename InputIterator>
+        void addNumbers(InputIterator begin, InputIterator end) {
+            size_t distance = std::distance(begin, end);
+            if (this->_storedCount + distance > this->_maxStorage) {
+                throw std::out_of_range("Not enough space in Span to add range!");
+            }
+            this->_storedCount.insert(this->_storedCount.end(), begin, end);
+}
+};
+
